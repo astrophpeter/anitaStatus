be4b2eecfb5d5ef937d29f844c37f691b1b3b942
Peter McGill <petermcgill94@gmail.com>
2016-11-06 19:19:30 +0000

    as

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 var app = angular.module("app", ["chart.js"]);
 
-app.factory('simpleFactory', function($https) {
+app.factory('simpleFactory', function($q, $https) {
   //var tempData = [
     //[65, 59, 80, 81, 56, 55, 40],
     //[28, 48, 40, 19, 86, 27, 90]
